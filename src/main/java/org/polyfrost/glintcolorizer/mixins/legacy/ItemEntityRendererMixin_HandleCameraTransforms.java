0a0238fbebcada2845e1d8b0d2904ba8d34ee16b
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-09-13 16:36:09 -0400

    Target correct render method in ItemEntityRendererMixin

--- src/main/java/org/polyfrost/glintcolorizer/mixins/legacy/ItemEntityRendererMixin_HandleCameraTransforms.java
+++ src/main/java/org/polyfrost/glintcolorizer/mixins/legacy/ItemEntityRendererMixin_HandleCameraTransforms.java
@@ -5,17 +5,19 @@ package org.polyfrost.glintcolorizer.mixins.legacy;
 
 import net.minecraft.client.render.entity.ItemEntityRenderer;
 import net.minecraft.client.render.model.block.ModelTransformations;
+import net.minecraft.entity.ItemEntity;
 import org.polyfrost.glintcolorizer.util.legacy.RenderItemHook;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 
 
 @Mixin(ItemEntityRenderer.class)
 public class ItemEntityRendererMixin_HandleCameraTransforms {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/block/ModelTransformations;apply(Lnet/minecraft/client/render/model/block/ModelTransformations$Type;)V", ordinal = 0))
-    private void glintcolorizer$setCameraTransform() {
+    @Inject(method = "render(Lnet/minecraft/entity/ItemEntity;DDDFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/model/block/ModelTransformations;apply(Lnet/minecraft/client/render/model/block/ModelTransformations$Type;)V", ordinal = 0))
+    private void glintcolorizer$setCameraTransform(ItemEntity itemEntity, double d, double e, double f, float g, float h, CallbackInfo ci) {
         RenderItemHook.INSTANCE.transformType = ModelTransformations.Type.GROUND;
     }
 }
