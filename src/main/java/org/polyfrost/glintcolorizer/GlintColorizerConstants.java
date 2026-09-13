b7a5175c1b9626993ed2a6cdd476d50a299148ad
Yedelo <158670400+yedelo@users.noreply.github.com>
2026-09-13 16:25:16 -0400

    Fix mod id constant not working

--- src/main/java/org/polyfrost/glintcolorizer/GlintColorizerConstants.java
+++ src/main/java/org/polyfrost/glintcolorizer/GlintColorizerConstants.java
@@ -2,7 +2,7 @@ package org.polyfrost.glintcolorizer;
 
 public final class GlintColorizerConstants {
 	// Sets the variables from `gradle.properties`. Depends on the `bloom` gradle plugin.
-	public static final String ID = "@MODID@";
+	public static final String ID = "@MOD_ID@";
 	public static final String NAME = "@MOD_NAME@";
 	public static final String VERSION = "@MOD_VERSION@";
 }
